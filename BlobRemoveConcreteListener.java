102fad962a896a91f175ff20ca94cd9a4cb74a05
Rajkumar Singh <rsmranal2@gmail.com>
2014-06-05 13:21:41 -0400

    Create BlobRemoveConcreteListener.java
    This removes blob object

---
+++ BlobRemoveConcreteListener.java
@@ -0,0 +1,27 @@
+package com.qubole;
+/**
+ *
+ *@author Raj
+ */
+import java.io.File;
+
+public class BlobRemoveConcreteListener implements BlobRemoveListner {
+
+        BlobStore blobStr;
+
+        public BlobRemoveConcreteListener(BlobStore blobStore) {
+                this.blobStr = blobStore;
+        }
+
+        @Override
+        public void removeObject(BlobWrapper blob, boolean removeFromSha1) {
+                // remove File from Storage
+                File file = new File(blob.getObjectPath());
+                file.delete();
+                System.out.println("Deleting File :" + blob.objectPath);
+                // remove from sha1map
+                if (removeFromSha1) {
+                        blobStr.removeFromSha1(blob);
+                }
+        }
+}
